ec9f01523791cd04e716396278644042baf262c8
Mario Granados <mario.alberto.granados1@gmail.com>
2023-01-04 11:59:30 -0600

    queries

--- src/main/java/com/poke/pokeApp/Controllers/UserController.java
+++ src/main/java/com/poke/pokeApp/Controllers/UserController.java
@@ -1,20 +1,23 @@
 package com.poke.pokeApp.Controllers;
+
 import java.util.List;
 import java.util.Optional;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poke.pokeApp.Models.User;
 import com.poke.pokeApp.Repo.UserRepo;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     private final UserRepo userRepo;
@@ -38,6 +41,11 @@ public class UserController {
     Optional<User> user(@PathVariable Long id) {
 
         return userRepo.findById(id);
+    }
+
+    @GetMapping("/users/findUser/{username}+{password}")
+    User userLogin(@RequestParam String username, @RequestParam String password) {
+        return userRepo.findUserByUsernameAndPassword(username, password);
     }
 
     @PutMapping("/users/{id}")
--- src/main/java/com/poke/pokeApp/Repo/UserRepo.java
+++ src/main/java/com/poke/pokeApp/Repo/UserRepo.java
@@ -3,11 +3,14 @@ package com.poke.pokeApp.Repo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.poke.pokeApp.Models.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-    
+    @Query(value = "SELECT * FROM users WHERE username = ?1 AND password = ?2 LIMIT 1", nativeQuery = true)
+    User findUserByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
